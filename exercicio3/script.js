f4572f146faa0b2a699969d83101e8d71d6fca09
Roberto Jesus <roliberdade1433@gmail.com>
2023-12-13 21:17:20 -0300

    exercicio3

---
+++ exercicio3/script.js
@@ -0,0 +1,8 @@
+const cincoPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil", "Nadanda","bababab"]
+
+let posicao = 1
+for (const pais of cincoPaises){
+    console.log(`${posicao} - ${pais}`)
+    posicao++
+
+}
